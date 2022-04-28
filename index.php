b05505fab3bfe665796908da47a530ad09a34a1e
Siwek <norbert.siwek@live.zs1mm.edu.pl>
2022-04-28 08:34:41 +0200

    wersja_0.1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    echo '<!DOCTYPE html>';
+    echo ' <html lang="en">';
+    echo ' <head>';
+    echo '     <meta charset="UTF-8">';
+    echo '     <meta http-equiv="X-UA-Compatible" content="IE=edge">';
+    echo '     <meta name="viewport" content="width=device-width, initial-scale=1.0">';
+    echo '     <title>Siwek</title>';
+    echo ' </head>';
+    echo ' <body>';
+    echo '     <h1>Strona w przygotowaniu</h1>';
+    echo date('H:i:s d/m/Y');
+    echo ' </body>';
+    echo ' </html>';
+    
+?>
